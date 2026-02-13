6a6e97563a11c9a46e4bcaf9f7d411a62a109014
Riki Fujihara <rikifujihara@gmail.com>
2026-02-13 11:59:05 +1100

    TDD: new test - NewCardForm.tsx does not allow duplicate card name within the same column

--- src/components/NewCardForm.test.tsx
+++ src/components/NewCardForm.test.tsx
@@ -1,14 +1,30 @@
-import { render, within } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { expect, test } from "vitest";
 import Board from "./Board";
-import { addColumn } from "../test/helpers";
+import { addCard, addColumn } from "../test/helpers";
 
 test("Focuses input when clicking add card button", async () => {
   const user = userEvent.setup();
   render(<Board />);
+
   const column = await addColumn(user, "Todo");
   await user.click(within(column).getByRole("button", { name: /add card/i }));
   const input = within(column).getByLabelText(/card name/i);
+
   expect(input).toHaveFocus();
+});
+
+test("Does not allow duplicate card names within column", async () => {
+  const user = userEvent.setup();
+  render(<Board />);
+
+  const column = await addColumn(user, "Todo");
+  await addCard(user, "Todo", "Walk the dog");
+  await user.click(within(column).getByRole("button", { name: /add card/i }));
+  await user.type(within(column).getByLabelText(/card name/i), "Walk the dog");
+  await user.click(within(column).getByRole("button", { name: /add card/i }));
+
+  const cards = within(column).getAllByText("Walk the dog");
+  expect(cards.length).toBe(1);
 });
